4632bde3d64249cfa6af89d43776bd91faca8e95
Renzoo <lbafable@up.edu.ph>
2020-04-11 21:22:27 +0800

    Remove usage of config file

--- utils/currentWeather.js
+++ utils/currentWeather.js
@@ -2,7 +2,7 @@ const request = require('request');
 const config = require('../config');
 
 const currentWeather = (lon, lat, callback) => {
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${config.openWeatherMapKey}`;
+  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=af53d9ed4492e68a147204d1634d1421`;
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
